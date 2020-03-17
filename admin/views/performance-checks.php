2eb0a727895167b90a2e3d1bca4535ffe966bf2b
Robert Sæther <robsat91@gmail.com>
2020-03-17 13:04:44 +0100

    Added helptext regarding WP cron with unix cron

--- admin/views/performance-checks.php
+++ admin/views/performance-checks.php
@@ -131,7 +131,7 @@
             <?php if ( $wp_cron_disabled === true ) : ?>
             <div><img src="<?php echo SERVEBOLT_PATH_URL; ?>admin/assets/img/checked.png" width="20"></div> <?php sb_e('WP Cron is disabled. Remember to set on cron on the server.'); ?>
             <?php else : ?>
-            <div><img src="<?php echo SERVEBOLT_PATH_URL; ?>admin/assets/img/cancel.png" width="20"></div> <?php sb_e('WP Cron is enabled, and may slow down your site and/or degrade the sites ability to scale. This should be disabled and run with server cron.'); ?>
+            <div><img src="<?php echo SERVEBOLT_PATH_URL; ?>admin/assets/img/cancel.png" width="20"></div> <?php sb_e('WP Cron is enabled, and may slow down your site and/or degrade the sites ability to scale. This should be disabled and run with server cron. Read more about this <a href="https://developer.wordpress.org/plugins/cron/hooking-wp-cron-into-the-system-task-scheduler/" style="margin-left: 3px;" target="_blank">here</a>.'); ?>
             <?php endif;?>
           </div>
 				</td>
