07d5e7af9d1812df679182dc9690488cb955bbb2
Robert Sæther <robsat91@gmail.com>
2020-06-03 13:04:57 +0200

    Renamed JS-files

--- admin/admin-interface.php
+++ admin/admin-interface.php
@@ -106,7 +106,7 @@ class Servebolt_Admin_Interface {
 	 */
 	public function plugin_scripts() {
 		wp_enqueue_script( 'servebolt-optimizer-scripts', SERVEBOLT_PATH_URL . 'admin/assets/js/general.js', [], filemtime(SERVEBOLT_PATH . 'admin/assets/js/general.js'), true );
-		wp_enqueue_script( 'servebolt-optimizer-cloudflare-cache-purge-scripts', SERVEBOLT_PATH_URL . 'admin/assets/js/cloudflare-cache-purge.js', ['servebolt-optimizer-scripts'], filemtime(SERVEBOLT_PATH . 'admin/assets/js/cloudflare-cache-purge.js'), true );
+		wp_enqueue_script( 'servebolt-optimizer-cloudflare-cache-purge-trigger-scripts', SERVEBOLT_PATH_URL . 'admin/assets/js/cloudflare-cache-purge-trigger.js', ['servebolt-optimizer-scripts'], filemtime(SERVEBOLT_PATH . 'admin/assets/js/cloudflare-cache-purge-trigger.js'), true );
 		wp_enqueue_script( 'sb-sweetalert2', SERVEBOLT_PATH_URL . 'admin/assets/js/sweetalert2.all.min.js', ['servebolt-optimizer-scripts'], filemtime(SERVEBOLT_PATH . 'admin/assets/js/sweetalert2.all.min.js'), true );
 		wp_localize_script( 'servebolt-optimizer-scripts', 'ajax_object', ['ajax_nonce' => sb_get_ajax_nonce()] );
 	}
--- admin/cf-cache-admin-controls.php
+++ admin/cf-cache-admin-controls.php
@@ -57,7 +57,7 @@ class CF_Cache_Admin_Controls {
 	public function plugin_scripts() {
 		$screen = get_current_screen();
 		if ( $screen->id != 'servebolt_page_servebolt-cf-cache-control' ) return;
-		wp_enqueue_script( 'servebolt-optimizer-cloudflare-scripts', SERVEBOLT_PATH_URL . 'admin/assets/js/cloudflare.js', ['servebolt-optimizer-scripts'], filemtime(SERVEBOLT_PATH . 'admin/assets/js/cloudflare.js'), true );
+		wp_enqueue_script( 'servebolt-optimizer-cloudflare-cache-purge-scripts', SERVEBOLT_PATH_URL . 'admin/assets/js/cloudflare-cache-purge.js', ['servebolt-optimizer-scripts'], filemtime(SERVEBOLT_PATH . 'admin/assets/js/cloudflare-cache-purge.js'), true );
 	}
 
 	/**
