46ccf99214af2516614b8378b5e59e88ba85ca4e
Robert Sæther <robert@servebolt.com>
2021-02-18 12:09:46 +0100

    Bugfix in shonky settings system

--- cli/classes/general-settings.class.php
+++ cli/classes/general-settings.class.php
@@ -14,19 +14,39 @@ class Servebolt_CLI_General_Settings extends Servebolt_CLI_General_Settings_Extr
      *
      * ## OPTIONS
      *
+     * [--all]
+     * : Display the setting for all sites.
+     *
      * ## EXAMPLES
      *
-     *     wp servebolt general-settings list
+     *     wp servebolt general-settings list --all
      *
      */
     public function command_general_settings_list($args, $assoc_args) {
-        $columns = [ 'Name', 'Type' ];
-        $items = array_map(function($item) use ($columns) {
-            return array_combine($columns, $item);
-        }, $this->get_settings());
-        WP_CLI::line(sprintf('%s setting(s):', count($items)));
-        WP_CLI\Utils\format_items( 'table', $items, $columns);
-        WP_CLI::line('');
+        $columns = [ 'Name', 'Type', 'Value' ];
+        if ( $this->affect_all_sites($assoc_args) ) {
+            sb_iterate_sites(function ($site) use ($columns) {
+                $items = array_map(function($item) use ($columns, $site) {
+                    $settings_key = $this->resolve_settings_key($item['name']);
+                    $resolve_settings_values = $this->get_setting($settings_key, $site->blog_id);
+                    $item['value'] = $resolve_settings_values[$settings_key];
+                    return array_combine($columns, $item);
+                }, $this->get_settings());
+                WP_CLI::line(sprintf('%s setting(s) for site "%s":', count($items), get_site_url($site->blog_id)));
+                WP_CLI\Utils\format_items( 'table', $items, $columns);
+                WP_CLI::line('');
+            });
+        } else {
+            $items = array_map(function($item) use ($columns) {
+                $settings_key = $this->resolve_settings_key($item['name']);
+                $resolve_settings_values = $this->get_setting($settings_key);
+                $item['value'] = $resolve_settings_values[$settings_key];
+                return array_combine($columns, $item);
+            }, $this->get_settings());
+            WP_CLI::line(sprintf('%s setting(s):', count($items)));
+            WP_CLI\Utils\format_items( 'table', $items, $columns);
+            WP_CLI::line('');
+        }
         WP_CLI::line(sb__('Use "wp servebolt general-settings get [name]" and "wp servebolt general-settings set [name]" to get/set value of a settings.'));
     }
 
@@ -65,7 +85,7 @@ class Servebolt_CLI_General_Settings extends Servebolt_CLI_General_Settings_Extr
     }
 
     /**
-     * Get the value of a setting.
+     * Set the value of a setting.
      *
      * ## OPTIONS
      *
