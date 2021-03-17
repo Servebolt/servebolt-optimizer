4a8baa8e8c37a0db58cc6f36aac341cedd27380d
Robert Sæther <robert@servebolt.com>
2021-03-17 14:33:48 +0100

    Adjusted cleanup-script for deployment

--- cleanup.sh
+++ cleanup.sh
@@ -6,7 +6,6 @@ rm -rf tests 2> /dev/null
 rm -rf bin 2> /dev/null
 rm -rf phpunit.xml.dist 2> /dev/null
 rm -rf node_modules 2> /dev/null
-rm -rf vendor 2> /dev/null
 rm -rf .babelrc 2> /dev/null
 rm -rf .eslintignore 2> /dev/null
 rm -rf .eslintrc 2> /dev/null
@@ -16,4 +15,6 @@ rm -rf package.json 2> /dev/null
 rm -rf postcss.config.js 2> /dev/null
 rm -rf webpack.config.js 2> /dev/null
 rm -rf yarn.lock 2> /dev/null
+rm -rf tests 2> /dev/null
+rm -rf ci 2> /dev/null
 
