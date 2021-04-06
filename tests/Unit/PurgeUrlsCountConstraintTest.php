d1700cf0517fb475781135b84233fa665ab8e4a3
Robert Sæther <robert@servebolt.com>
2021-04-06 15:49:27 +0200

    Added test

---
+++ tests/Unit/PurgeUrlsCountConstraintTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Unit;
+
+use ServeboltWPUnitTestCase;
+
+/**
+ * Class QueueTest
+ * @package Unit\Queue
+ */
+class PurgeUrlsCountConstraintTest extends ServeboltWPUnitTestCase
+{
+
+}
