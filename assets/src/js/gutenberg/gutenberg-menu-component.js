8c56d72c4c1f43a88996f0c7e3ce8463eddd7c99
Robert Sæther <robert@servebolt.com>
2021-04-15 15:54:09 +0200

    Slight update in Gutenberg Plugin Sidebars

--- assets/src/js/gutenberg/gutenberg-menu-component.js
+++ assets/src/js/gutenberg/gutenberg-menu-component.js
@@ -1,75 +1,83 @@
-const { _x } = wp.i18n;
+const { _x, _n, sprintf } = wp.i18n;
 const { Fragment } = wp.element;
 const { PluginSidebar, PluginSidebarMoreMenuItem } = wp.editPost;
 const {
-	Button,
+  Button,
 } = wp.components;
 
 /**
- * Check whether a post status is considered public.
- *
- * @param status
- * @returns {boolean}
+ * Check whether the post status is purgeable.
+ * @param {string} status The status to be checked
+ * @returns {boolean} Whether the post status is considered purgeable.
  */
-function statusIsPublic(status) {
-	var non_cache_purgeable_post_statuses = ['auto-draft'];
-	return ! non_cache_purgeable_post_statuses.includes(status);
-	//var public_post_statuses = ['publish'];
-	//return public_post_statuses.includes(status);
+function statusIsPurgeable(status) {
+  const nonCachePurgeablePostStatuses = [ 'auto-draft', 'draft' ];
+  return ! nonCachePurgeablePostStatuses.includes(status);
 }
 
 /**
  * Get the initial post status.
- *
- * @returns {*}
+ * @returns {*} Initial post status.
  */
 function getInitialPostStatus() {
-	const element = document.getElementById('original_post_status');
-	return element ? element.value : false;
+  const element = document.getElementById('original_post_status');
+  return element ? element.value : false;
 }
 
 /**
  * Get the current post status.
- *
- * @returns {*}
+ * @returns {*} The current post status.
  */
 function getCurrentPostStatus() {
-	return wp.data.select( 'core/editor' ).getEditedPostAttribute( 'status' );
+  return wp.data.select('core/editor').getEditedPostAttribute('status');
 }
 
-let isPublic = statusIsPublic(getInitialPostStatus());
+/**
+ * Get singular post type name.
+ * @returns {boolean|*}
+ */
+function getPostTypeName() {
+    const editor = wp.data.select('core/editor'),
+        _select2 = wp.data.select('core'),
+        getPostType = _select2.getPostType,
+        postTypeObject = getPostType(editor.getCurrentPostType());
+    if (postTypeObject && postTypeObject.labels && postTypeObject.labels.singular_name) {
+        return postTypeObject.labels.singular_name;
+    }
+    return false;
+}
 
 /**
  * Listen for events in the editor and check if the post gets published/unpublished.
  */
-wp.data.subscribe(function () {
-	isPublic = statusIsPublic(getCurrentPostStatus());
+wp.data.subscribe(function() {
+  isPurgeable = statusIsPurgeable(getCurrentPostStatus());
 });
 
-const GutenbergMenuComponent = () => (
+let postType = 'post';
+let isPurgeable = statusIsPurgeable(getInitialPostStatus());
 
-	// Every component must have only one parent element.
-	// In this case, the parent element is a Fragment.
-	<Fragment>
-		<PluginSidebarMoreMenuItem target="sidebar-name">
-			{ _x( 'Servebolt Optimizer', 'text', 'sb-optimizer' ) }
-		</PluginSidebarMoreMenuItem>
-		<PluginSidebar name="sidebar-name" className="servebolt-optimizer-cache-bust-panel" title="Servebolt Optimizer">
+const ServeboltCachePurgeMenuComponent = () => (
+    <Fragment>
+        <PluginSidebarMoreMenuItem target="sidebar-name">
+            { _x('Servebolt Optimizer', 'text', 'sb-optimizer') }
+        </PluginSidebarMoreMenuItem>
+        <PluginSidebar name="sidebar-name" className="servebolt-optimizer-cache-bust-panel" title="Servebolt Optimizer">
+            { isPurgeable && postType &&
+            <Button className="sb-button yellow" onClick={ () => window.sb_purge_post_cache_with_auto_resolve() }>
+                { sprintf(_n('Purge %s cache', 'sb-optimizer'), postType) }
+            </Button>
+            }
+            <Button className="sb-button yellow" onClick={ () => window.sb_purge_all_cache() }>
+                { _x( 'Purge all cache', 'text', 'sb-optimizer' ) }
+            </Button>
 
-			{ isPublic &&
-				<Button className="sb-button yellow" onClick={ () => window.sb_purge_post_cache_with_auto_resolve() }>
-					{ _x( 'Purge current post cache', 'text', 'sb-optimizer' ) }
-				</Button>
-			}
-			<Button className="sb-button yellow" onClick={ () => window.sb_purge_all_cache() }>
-				{ _x( 'Purge all cache', 'text', 'sb-optimizer' ) }
-			</Button>
+            <Button className="sb-button yellow" onClick={ () => window.sb_purge_url_cache() }>
+                { _x( 'Purge a URL', 'text', 'sb-optimizer' ) }
+            </Button>
 
-			<Button className="sb-button yellow" onClick={ () => window.sb_purge_url_cache() }>
-				{ _x( 'Purge a URL', 'text', 'sb-optimizer' ) }
-			</Button>
-
-		</PluginSidebar>
-	</Fragment>
+        </PluginSidebar>
+    </Fragment>
 );
-export default GutenbergMenuComponent;
+
+export default ServeboltCachePurgeMenuComponent;
--- assets/src/js/gutenberg/gutenberg-menu.js
+++ assets/src/js/gutenberg/gutenberg-menu.js
@@ -1,13 +1,11 @@
 import '../../css/gutenberg-menu.scss';
 
-import GutenbergMenuComponent from './gutenberg-menu-component.js';
-import ElementIcon from '../../images/servebolt-icon.svg';
+import ServeboltCachePurgeMenuComponent from './gutenberg-menu-component.js';
+import ServeboltCachePurgeMenuElementIcon from '../../images/servebolt-icon.svg';
 
 const { registerPlugin } = wp.plugins;
 
-// More info:
-// https://wordpress.org/gutenberg/handbook/designers-developers/developers/packages/packages-plugins/
-registerPlugin( 'servebolt-optimizer', {
-	icon: <ElementIcon />,
-	render: GutenbergMenuComponent,
-} );
+registerPlugin('servebolt-optimizer', {
+  icon: <ServeboltCachePurgeMenuElementIcon />,
+  render: ServeboltCachePurgeMenuComponent,
+});
