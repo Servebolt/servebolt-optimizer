53cd24eb7239f3ecf096b184c74ca5d54bd5f92b
Robert Sæther <robert@servebolt.com>
2021-04-29 10:16:34 +0200

    Added support for CLI when preventing plugin from being active on a single site in a multisite setup

--- src/Servebolt/PluginActiveStateHandling.php
+++ src/Servebolt/PluginActiveStateHandling.php
@@ -4,9 +4,11 @@ namespace Servebolt\Optimizer;
 
 if (!defined('ABSPATH')) exit; // Exit if accessed directly
 
+use WP_CLI;
 use Servebolt\Optimizer\Utils\DatabaseMigration\MigrationRunner;
 use function Servebolt\Optimizer\Helpers\cacheCookieCheck;
 use function Servebolt\Optimizer\Helpers\clearNoCacheCookie;
+use function Servebolt\Optimizer\Helpers\isCli;
 
 /**
  * Class PluginActiveStateHandling
@@ -41,12 +43,30 @@ class PluginActiveStateHandling
     }
 
     /**
+     * Check whether we should prevent plugin from being active.
+     *
+     * @return bool
+     */
+    private function shouldPreventSingleSiteActivation(): bool
+    {
+        return is_multisite() && !is_plugin_active_for_network(SERVEBOLT_PLUGIN_BASENAME);
+    }
+
+    /**
      * Prevent the plugin from being activated on a single site when running in a multisite-environment.
      */
     private function preventSingleSitePluginActivationInMultisite(): void
     {
+        if (isCli()) {
+            add_action('activate_' . SERVEBOLT_PLUGIN_BASENAME, function($network_wide) {
+                if (!$network_wide && $this->shouldPreventSingleSiteActivation()) {
+                    WP_CLI::error(__('Servebolt Optimizer can only be network activated when running on a multisite.', 'servebolt-wp'));
+                    die;
+                }
+            }, 10, 1);
+        }
         add_action('admin_init', function () {
-            if (is_multisite() && !is_plugin_active_for_network(SERVEBOLT_PLUGIN_BASENAME)) {
+            if ($this->shouldPreventSingleSiteActivation()) {
                 deactivate_plugins(SERVEBOLT_PLUGIN_BASENAME);
                 if (isset($_GET['activate'])) {
                     unset($_GET['activate']);
@@ -55,7 +75,7 @@ class PluginActiveStateHandling
                 add_action('admin_notices', function() {
                     ?>
                     <div class="notice notice-warning is-dismissable">
-                        <p><?php printf(__('Servebolt Optimizer can only be network activated when running on a multisite.', 'servebolt-wp'), phpversion()); ?></p>
+                        <p><?php echo __('Servebolt Optimizer can only be network activated when running on a multisite.', 'servebolt-wp'); ?></p>
                     </div>
                     <?php
                 });
