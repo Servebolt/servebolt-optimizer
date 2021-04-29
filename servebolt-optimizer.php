38cef63163e79341f1ff0f46de8f064907bc0120
Robert Sæther <robert@servebolt.com>
2021-04-29 14:18:15 +0200

    Updated version no

--- servebolt-optimizer.php
+++ servebolt-optimizer.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Servebolt Optimizer
-Version: 2.2.0-beta.23
+Version: 2.2.0-beta.24
 Author: Servebolt
 Author URI: https://servebolt.com
 Description: A plugin that implements Servebolt Security & Performance best practises for WordPress.
