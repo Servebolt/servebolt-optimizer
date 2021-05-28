7d4002937105823a9fa0adc62f13a5a054bb4770
Robert Sæther <robert@servebolt.com>
2021-05-28 13:49:13 +0200

    Minor tweak

--- src/Servebolt/Views/accelerated-domains/accelerated-domains.php
+++ src/Servebolt/Views/accelerated-domains/accelerated-domains.php
@@ -1,7 +1,4 @@
 <?php if (!defined('ABSPATH')) exit; // Exit if accessed directly
-?>
-<?php
-
 use function Servebolt\Optimizer\Helpers\view;
 use function Servebolt\Optimizer\Helpers\getServeboltAdminUrl;
 ?>
