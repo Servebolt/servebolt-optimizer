b51f8bd5eed0f17e799bac034c54a83bf3cfc2a0
Robert Sæther <robert@servebolt.com>
2021-06-10 15:13:33 +0200

    Updated local build folder name

--- ci/local-build.sh
+++ ci/local-build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-mkdir build
-rsync -av --progress ./ build --exclude build --exclude vendor --exclude node_modules --exclude tests --exclude .git
-cd build
+mkdir servebolt-optimizer
+rsync -av --progress ./ servebolt-optimizer --exclude servebolt-optimizer --exclude vendor --exclude node_modules --exclude tests --exclude .git
+cd servebolt-optimizer
 composer install --no-dev --optimize-autoloader
 yarn install
 yarn production
 ./ci/cleanup.sh
 cd ..
-zip -r -9 servebolt-optimizer.zip build
-rm -rf build
+zip -r -9 servebolt-optimizer.zip servebolt-optimizer
+rm -rf servebolt-optimizer
