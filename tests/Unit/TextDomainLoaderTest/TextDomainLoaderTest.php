9b7538aad04d1130540e8d707c7ecd0144a650a1
Robert Sæther <robert@servebolt.com>
2021-07-05 19:13:37 +0200

    Added tests for custom text domain loader

--- tests/Unit/TextDomainLoaderTest/TextDomainLoaderTest.php
+++ tests/Unit/TextDomainLoaderTest/TextDomainLoaderTest.php
@@ -12,11 +12,14 @@ class TextDomainLoaderTest extends ServeboltWPUnitTestCase
 
     public function testThatCustomLoaderWorks()
     {
-        // TODO: Set language to nb_NO
-        $this->assertEquals('This is a test', __('This is a test', 'sb-optimizer-test'));
+        $textDomain = 'sb-optimizer-test';
+        $this->assertEquals('An example', __('An example', $textDomain));
+        switch_to_locale('es_ES');
+        $this->assertEquals('An example', __('An example', $textDomain));
         add_filter('sb_optimizer_get_option_servebolt_custom_text_domain_loader_switch', '__return_true');
         new WpTextDomainLoader;
-        // TODO: Load MO-file
-        $this->assertEquals('Dette er en test', __('This is a test', 'sb-optimizer-test'));
+        $this->assertEquals(1, has_filter('override_load_textdomain', 'Servebolt\Optimizer\TextDomainLoader\WpTextDomainLoader::aFasterLoadTextDomain'));
+        load_textdomain($textDomain, __DIR__ . '/test-es_ES.mo');
+        $this->assertEquals('Un ejemplo', __('An example', $textDomain));
     }
 }
