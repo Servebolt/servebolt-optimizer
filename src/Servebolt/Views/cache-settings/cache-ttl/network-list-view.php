104aae11178bd936a3a0c1b93750f9bca78f378e
Robert Sæther <robert@servebolt.com>
2021-07-13 15:45:30 +0200

    Corrected multisite Cache TTL template

--- src/Servebolt/Views/cache-settings/cache-ttl/network-list-view.php
+++ src/Servebolt/Views/cache-settings/cache-ttl/network-list-view.php
@@ -1,15 +1,11 @@
 <?php if (!defined('ABSPATH')) exit; // Exit if accessed directly ?>
-<?php use function Servebolt\Optimizer\Helpers\formatPostTypeSlug; ?>
-<?php use Servebolt\Optimizer\FullPageCache\FullPageCacheHeaders; ?>
-<?php use Servebolt\Optimizer\FullPageCache\FullPageCacheSettings; ?>
+<br>
 
 <table class="wp-list-table widefat striped">
     <thead>
     <tr>
         <th><?php _e('Blog ID', 'servebolt-wp'); ?></th>
         <th><?php _e('URL', 'servebolt-wp'); ?></th>
-        <th><?php _e('Full Page Cache Active', 'servebolt-wp'); ?></th>
-        <th><?php _e('Post types', 'servebolt-wp'); ?></th>
         <th><?php _e('Controls', 'servebolt-wp'); ?></th>
     </tr>
     </thead>
@@ -17,32 +13,15 @@
     <tr>
         <th><?php _e('Blog ID', 'servebolt-wp'); ?></th>
         <th><?php _e('URL', 'servebolt-wp'); ?></th>
-        <th><?php _e('Full Page Cache Active', 'servebolt-wp'); ?></th>
-        <th><?php _e('Post types', 'servebolt-wp'); ?></th>
         <th><?php _e('Controls', 'servebolt-wp'); ?></th>
     </tr>
     </tfoot>
     <tbody>
     <?php foreach (get_sites() as $site) : ?>
-        <?php $sbFpcSettings = FullPageCacheHeaders::getPostTypesToCache(false, false, $site->blog_id); ?>
         <tr>
             <td><?php echo $site->blog_id; ?></td>
             <td><?php echo $site->domain . $site->path; ?></td>
-            <td><?php echo FullPageCacheSettings::fpcIsActive($site->blog_id) ? __('Yes', 'servebolt-wp') : __('No', 'servebolt-wp'); ?></td>
-            <td>
-                <?php if ( ! empty($sbFpcSettings) ) : ?>
-                    <?php if ( in_array('all', $sbFpcSettings) ) : ?>
-                        All
-                    <?php else: ?>
-                        <?php foreach ($sbFpcSettings as $postType) : ?>
-                            <?php echo formatPostTypeSlug($postType) . '<br>'; ?>
-                        <?php endforeach; ?>
-                    <?php endif; ?>
-                <?php else : ?>
-                    None
-                <?php endif; ?>
-            </td>
-            <td><a href="<?php echo get_admin_url( $site->blog_id, 'admin.php?page=servebolt-fpc' ); ?>" class="button btn"><?php _e('Go to site FPC settings', 'servebolt-wp'); ?></a></td>
+            <td><a href="<?php echo get_admin_url( $site->blog_id, 'admin.php?page=servebolt-cache-ttl' ); ?>" class="button btn"><?php _e('Go to site cache TTL settings', 'servebolt-wp'); ?></a></td>
         </tr>
     <?php endforeach; ?>
     </tbody>
