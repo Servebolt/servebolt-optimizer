8767a649a2eed8013c8965cffab05aff356d2a7c
Robert Sæther <robert@servebolt.com>
2021-07-13 15:46:41 +0200

    Corrected multisite Cache TTL template

--- src/Servebolt/Views/cache-settings/cache-ttl/network-list-view.php
+++ src/Servebolt/Views/cache-settings/cache-ttl/network-list-view.php
@@ -1,11 +1,12 @@
 <?php if (!defined('ABSPATH')) exit; // Exit if accessed directly ?>
-<br>
+<?php use Servebolt\Optimizer\FullPageCache\CacheTtl; ?>
 
 <table class="wp-list-table widefat striped">
     <thead>
     <tr>
         <th><?php _e('Blog ID', 'servebolt-wp'); ?></th>
         <th><?php _e('URL', 'servebolt-wp'); ?></th>
+        <th><?php _e('Custom cache TTL Active', 'servebolt-wp'); ?></th>
         <th><?php _e('Controls', 'servebolt-wp'); ?></th>
     </tr>
     </thead>
@@ -13,14 +14,17 @@
     <tr>
         <th><?php _e('Blog ID', 'servebolt-wp'); ?></th>
         <th><?php _e('URL', 'servebolt-wp'); ?></th>
+        <th><?php _e('Custom cache TTL Active', 'servebolt-wp'); ?></th>
         <th><?php _e('Controls', 'servebolt-wp'); ?></th>
     </tr>
     </tfoot>
     <tbody>
     <?php foreach (get_sites() as $site) : ?>
+
         <tr>
             <td><?php echo $site->blog_id; ?></td>
             <td><?php echo $site->domain . $site->path; ?></td>
+            <td><?php echo CacheTtl::isActive($site->blog_id) ? __('Yes', 'servebolt-wp') : __('No', 'servebolt-wp'); ?></td>
             <td><a href="<?php echo get_admin_url( $site->blog_id, 'admin.php?page=servebolt-cache-ttl' ); ?>" class="button btn"><?php _e('Go to site cache TTL settings', 'servebolt-wp'); ?></a></td>
         </tr>
     <?php endforeach; ?>
