7301e862f70b94496ab609794396c50bfaa9b2dd
Robert Sæther <robert@servebolt.com>
2021-09-08 10:48:22 +0200

    Fixed issue with database migration runner so that it also runs in other contexts, like the CLI, so that it also runs whenever we activate/deactivate the plugin in CLI-context

--- src/Servebolt/WpDatabaseMigrations.php
+++ src/Servebolt/WpDatabaseMigrations.php
@@ -4,7 +4,11 @@ namespace Servebolt\Optimizer;
 
 if (!defined('ABSPATH')) exit; // Exit if accessed directly
 
+use function Servebolt\Optimizer\Helpers\isAjax;
+use function Servebolt\Optimizer\Helpers\isCli;
+use function Servebolt\Optimizer\Helpers\isCron;
 use function Servebolt\Optimizer\Helpers\isTesting;
+use function Servebolt\Optimizer\Helpers\isWpRest;
 
 /**
  * Class WpDatabaseMigrations
@@ -20,7 +24,13 @@ class WpDatabaseMigrations
         $method = 'Servebolt\\Optimizer\\Utils\\DatabaseMigration\\MigrationRunner::run';
         if (is_admin()) {
             add_action('admin_init', $method);
-        } elseif(isTesting()) {
+        } elseif(
+            isTesting()
+            || isCli()
+            || isCron()
+            || isAjax()
+            || isWpRest()
+        ) {
             add_action('init', $method);
         }
     }
