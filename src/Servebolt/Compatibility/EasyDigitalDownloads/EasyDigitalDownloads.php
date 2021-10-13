3b9c5edc55e15937c11c891562a56e0bb179e050
Robert Sæther <robert@servebolt.com>
2021-10-13 15:53:20 +0200

    Added cache exception conditions for EDD page templates

--- src/Servebolt/Compatibility/EasyDigitalDownloads/EasyDigitalDownloads.php
+++ src/Servebolt/Compatibility/EasyDigitalDownloads/EasyDigitalDownloads.php
@@ -23,22 +23,6 @@ class EasyDigitalDownloads
         if (!easyDigitalDownloadsIsActive()) {
             return;
         }
-        add_action('template_redirect', function () {
-            if (edd_is_checkout()) {
-                echo '<h1>edd checkout</h1>';
-            }
-            if (edd_is_success_page()) {
-                echo '<h1>edd success page</h1>';
-            }
-            if (edd_is_failed_transaction_page()) {
-                echo '<h1>edd transaction page</h1>';
-            }
-            if (edd_is_purchase_history_page()) {
-                echo '<h1>edd purchase history page</h1>';
-            }
-            if (edd_is_purchase_history_page()) {
-                echo '<h1>edd purchase history page</h1>';
-            }
-        });
+        new CacheExceptionRules;
     }
 }
