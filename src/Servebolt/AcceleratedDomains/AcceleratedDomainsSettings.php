1c665ec42491e2364041db36a9ca043e6e1c923d
Robert Sæther <robert@servebolt.com>
2022-06-03 13:58:23 +0200

    Removed unused code

--- src/Servebolt/AcceleratedDomains/AcceleratedDomainsSettings.php
+++ src/Servebolt/AcceleratedDomains/AcceleratedDomainsSettings.php
@@ -4,9 +4,6 @@ namespace Servebolt\Optimizer\AcceleratedDomains;
 
 if (!defined('ABSPATH')) exit; // Exit if accessed directly
 
-use Servebolt\Optimizer\FullPageCache\FullPageCacheSettings;
-use function Servebolt\Optimizer\Helpers\getOption;
-use function Servebolt\Optimizer\Helpers\getOptionName;
 use function Servebolt\Optimizer\Helpers\listenForCheckboxOptionChange;
 
 /**
