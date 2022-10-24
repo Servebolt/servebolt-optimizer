3ab6c3f7e7ccb17cf508d95e2b3e98c57095d6b0
Andrew Killen <andrew@bas-works.com>
2022-10-24 07:37:46 +0200

    Adding UID and index to queue table

---
+++ src/Servebolt/Utils/DatabaseMigration/Migrations/5_AddUIDPlusIndexToQueueTable.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Servebolt\Optimizer\Utils\DatabaseMigration\Migrations;
+
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
+
+use Servebolt\Optimizer\Utils\DatabaseMigration\AbstractMigration;
+use Servebolt\Optimizer\Utils\DatabaseMigration\MigrationInterface;
+use function Servebolt\Optimizer\Helpers\tableHasIndex;
+use function Servebolt\Optimizer\Helpers\tableExists;
+
+/**
+ * Class AddParentIdIndexToQueueTable
+ * @package Servebolt\Optimizer\Utils\DatabaseMigration\Migrations
+ */
+class AddUIDPlusIndexToQueueTable extends AbstractMigration implements MigrationInterface
+{
+
+    /**
+     * Whether to use the function "dbDelta" when running queries.
+     *
+     * @var bool
+     */
+    protected $useDbDelta = false;
+
+    /**
+     * @var bool Whether the migration is active (optional, defaults to true if omitted).
+     */
+    public static $active = true;
+
+    /**
+     * @var string Table name (optional).
+     */
+    protected $tableName = 'sb_queue';
+
+    /**
+     * @var string The plugin version number that this migration belongs to.
+     */
+    public static $version = '3.5.10';
+
+    /**
+     * Migrate up.
+     * 
+     * Add new column UID, UID will be used to check if item has already be added to the queue 
+     * Add index to new colum
+     */
+    public function up(): void
+    {
+        $this->runSql('ALTER TABLE `%table-name%` ADD COLUMN `UID` VARCHAR(2083),  ADD INDEX `uid_index` (`UID`);');
+    }
+
+    /**
+     * Check whether the table has the index.
+     *
+     * @return bool
+     */
+    public function hasBeenRun($migrationMethod): bool
+    {
+        switch($migrationMethod) {
+            case 'up':
+                // if Table does NOT exit return TRUE, to stop processing.
+                // if Table exists and index exists return TRUE to stop processing
+                if (!tableExists($this->getTableNameWithPrefix()) || ( tableExists($this->getTableNameWithPrefix()) && tableHasIndex($this->getTableNameWithPrefix(), 'parent_id_index'))) {
+                    return true;
+                } 
+                // else return false so that processing can continue;
+                return false;
+
+                break;
+            case 'down':
+                // if Table does NOT exit return TRUE, to stop processing.
+                // if Table exists and index does not exist return TRUE to stop processing
+                if (!tableExists($this->getTableNameWithPrefix()) || ( tableExists($this->getTableNameWithPrefix()) && !tableHasIndex($this->getTableNameWithPrefix(), 'parent_id_index'))) {
+                    return true;
+                } 
+                // else return false so that processing can continue;
+                return false;
+                break;
+        }
+    }
+
+    /**
+     * Migrate down.
+     * 
+     * Drop the column and its index
+     */
+    public function down(): void
+    {
+        $this->runSql('ALTER TABLE `%table-name%` DROP COLUMN `UID` DROP INDEX `uid_index`');
+    }
+}
