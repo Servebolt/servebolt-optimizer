55d174748833bd40d82d458599f0322f2af986d3
Andrew Killen <andrew@bas-works.com>
2022-11-22 16:00:17 +0100

    releasing 3.5.10 with action scheduler enhancements

--- src/Servebolt/Compatibility/ActionScheduler/ActionScheduler.php
+++ src/Servebolt/Compatibility/ActionScheduler/ActionScheduler.php
@@ -17,6 +17,10 @@ class ActionScheduler
      */
     public function __construct()
     {
+        add_filter('action_scheduler_queue_runner_batch_size', [$this, 'batch_size']);
+        add_filter('action_scheduler_queue_runner_concurrent_batches', [$this, 'concurrent_batches']);
+        add_filter('action_scheduler_queue_runner_time_limit', [$this, 'time_limit']);
+
         if (!apply_filters('sb_optimizer_action_scheduler_compatibility', true)) {
             return;
         }
@@ -25,4 +29,32 @@ class ActionScheduler
         }
         new DisableActionSchedulerDefaultRunner;
     }
+
+    /**
+     * Increase default batch size from 25 to 100.
+     * @return int
+     */
+    public function batch_size($batch_size)
+    {
+        return 100;
+    }
+    
+    /**
+     * Increase number of batch run at once from 1 to 2.
+     * @return int
+     */
+    public function concurrent_batches($concurrent_batches)
+    {
+        return 2;
+    }
+    
+    /**
+     * Increase the timeout of a batch from 30 seconds to 55seconds.
+     * @return int
+     */
+    public function time_limit($time_limit)
+    {
+        return 55;
+    }
+
 }
