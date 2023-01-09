3bc77b8dbb94d40be1788017c89f9a25cd92538d
Andrew Killen <andrew@bas-works.com>
2023-01-09 09:04:41 +0100

    more cache tag headers to ACD testing

--- src/Servebolt/CacheTags/AddCacheTagsHeaders.php
+++ src/Servebolt/CacheTags/AddCacheTagsHeaders.php
@@ -109,7 +109,8 @@ class AddCacheTagsHeaders extends CacheTagsBase {
         if(count($this->headers) > 0) {
             try{
                 header('Cache-Tag: ' . implode(',', $this->headers));
-                header('X-Cache-Tag: ' . implode(',', $this->headers));
+                header('x-acd-Cache-Tag: ' . implode(',', $this->headers));
+                header('x-Cache-Tag: ' . implode(',', $this->headers));
             }
             catch (Exception $e){
                 error_log("Cache-Tag messages could not be added as headers have already been sent.");
