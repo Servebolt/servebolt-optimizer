694788539b78dc51a2332a5fa4c163d7d7d4c905
Andrew Killen <andrew@bas-works.com>
2023-04-24 12:53:28 +0200

    fixed depreciated errors from add_submenu_page creation

--- servebolt-optimizer.php
+++ servebolt-optimizer.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Servebolt Optimizer
-Version: 3.5.19
+Version: 3.5.20
 Author: Servebolt
 Author URI: https://servebolt.com
 Description: A plugin that implements Servebolt Security & Performance best practises for WordPress.
