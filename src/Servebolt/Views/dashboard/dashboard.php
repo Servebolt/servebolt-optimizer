afc64bcafa2d6a75e7c2b8f2ce341db709a19be3
Andrew Killen <andrew@bas-works.com>
2024-01-17 17:24:08 +0100

    change logo to new SVG with Retina capibility

--- src/Servebolt/Views/dashboard/dashboard.php
+++ src/Servebolt/Views/dashboard/dashboard.php
@@ -1,9 +1,11 @@
 <?php if (!defined('ABSPATH')) exit; // Exit if accessed directly ?>
 <?php use function Servebolt\Optimizer\Helpers\isHostedAtServebolt; ?>
 <?php use function Servebolt\Optimizer\Helpers\view; ?>
+<?php $image_path = trailingslashit(SERVEBOLT_PLUGIN_DIR_URL) . 'assets/dist/images/'; ?>
 
 <div class="wrap sb-content">
-	<div class="sb-logo"></div>
+    <img srcset="<?php $image_path ?>servebolt-logo.svg, <?php $image_path ?>servebolt-logo-retina.svg 2x" src="<?php $image_path ?>servebolt-logo.svg" alt="Servebolt logo" width="170px" height="30px">
+	<!-- <div class="sb-logo"></div> -->
 	<h1 class="heading"><?php _e('Performance Tools', 'servebolt-wp'); ?></h1>
 
     <?php settings_errors(); ?>
