427eac54814af0042fb3afd54f170448ef232970
Andrew Killen <andrew@bas-works.com>
2024-01-18 12:00:57 +0100

    changed migration number to force purge on ACD/CDN

--- servebolt-optimizer.php
+++ servebolt-optimizer.php
@@ -27,7 +27,7 @@ define('SERVEBOLT_PLUGIN_ACD_VERSION', '7');
  * when updating the plugin tables.
  * @since 3.5.11
  */
-define('SERVEBOLT_PLUGIN_DB_VERSION', 100);
+define('SERVEBOLT_PLUGIN_DB_VERSION', 101);
 
 // Abort and display WP admin notice if PHP version is less than constant SERVEBOLT_PLUGIN_MINIMUM_PHP_VERSION
 if (version_compare(phpversion(), SERVEBOLT_PLUGIN_MINIMUM_PHP_VERSION, '<')) {
