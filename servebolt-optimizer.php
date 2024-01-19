81e0921db1fe84e5e3807667b0bf509b6607aed3
Andrew Killen <andrew@bas-works.com>
2024-01-19 08:29:51 +0100

    preperation pre-release

--- servebolt-optimizer.php
+++ servebolt-optimizer.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Servebolt Optimizer
-Version: 3.5.26
+Version: 3.5.27
 Author: Servebolt
 Author URI: https://servebolt.com
 Description: A plugin that implements Servebolt Security & Performance best practises for WordPress.
